01675aa5dc69b9e4755a84176bc4ef1d4d1d1482
Umananda Siddha <umanandasiddha243@gmail.com>
2026-02-27 23:57:20 +0530

    fix: added auth to coupons

--- src/modules/coupon/coupon.module.ts
+++ src/modules/coupon/coupon.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { CouponService } from './coupon.service';
 import { CouponController } from './coupon.controller';
 import { DatabaseModule } from 'src/services/database/database.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-    imports: [DatabaseModule],
+    imports: [DatabaseModule, AuthModule],
     controllers: [CouponController],
     providers: [CouponService],
     exports: [CouponService], // BookingModule and others can inject CouponService
